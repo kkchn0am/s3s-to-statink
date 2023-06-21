b33263e04ce4ae0f48b0d83af4ce6b98dcc343c5
kkchn0am <135973628+kkchn0am@users.noreply.github.com>
2023-06-21 11:21:22 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
 curl -X POST -f -H "Authorization: Bearer ${DROPBOX_TOKEN}" \
     -D - -H "Dropbox-API-Arg: {\"path\": \"/${TIMESTAMP}.*\",\"mode\": \"overwrite\",\"mute\": false}" \
     -H "Content-Type: application/octet-stream" \
-    --data-binary @/log.txt \
+    --data-binary @log.txt \
     https://content.dropboxapi.com/2/files/upload
