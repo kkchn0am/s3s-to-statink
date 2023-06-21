d7d0b43d7584469b61c33ed0c7ae5452557712b6
kkchn0am <135973628+kkchn0am@users.noreply.github.com>
2023-06-21 14:32:12 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@ set -eux
 
 export TZ="Asia/Tokyo"
 
-TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
-
 curl -o /dev/null -s -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer ${DROPBOX_TOKEN}" \
     --header "Dropbox-API-Arg: {\"path\": \"/Splatoon3/$1/$2\",\"mode\": \"overwrite\",\"mute\": false}" \
     --header "Content-Type: application/octet-stream" \
