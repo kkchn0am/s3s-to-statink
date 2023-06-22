6c2afe5da2fd897cede9f603919656c27790d17a
kkchn0am <135973628+kkchn0am@users.noreply.github.com>
2023-06-22 12:38:36 +0900

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-set -eux
-
-export TZ="Asia/Tokyo"
-
-curl -o /dev/null -s -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer ${DROPBOX_TOKEN}" \
-    --header "Dropbox-API-Arg: {\"path\": \"/Splatoon3/$1/$2\",\"mode\": \"overwrite\",\"mute\": false}" \
-    --header "Content-Type: application/octet-stream" \
-    --data-binary @$2
